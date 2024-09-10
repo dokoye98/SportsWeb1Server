6744b9a3ef379c60070560328ea32f80d3f09864
Dokoye98 <Dwayneokoye98@gmail.com>
2024-09-10 22:52:56 +0100

    mongoose added

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const {restart} = require('nodemon')
 const app = express()
+const mongoose = require('mongoose')
+require('dotenv/config')
+
+
+mongoose.connect(process.env.DB_CONNECTOR).then(()=>{
+    console.log('DB connected')
+})
 
 
 app.listen(5000,()=>{
---
+++ tokenGen.js
@@ -0,0 +1,17 @@
+const jsonwebtoken = require('jsonwebtoken')
+
+function val(req,res,next){
+
+    const token = req.header('auth-token')
+    if(!token){
+        return res.status(400).send({message:'Access Denied'})
+    }
+    try{
+        const verified = jsonwebtoken.verify(token,process.env.TOKEN)
+        req.user = verified
+        next()
+    }catch(err){
+        return res.status(400).send({message:err})
+    }
+}
+module.exports = val
